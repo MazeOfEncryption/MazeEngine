4e038629177ed5459f11972b27486dbde605686a
MazeOfEncryption <inbox@encryptedmaze.net>
2019-09-18 14:26:58 -0400

    Minor Fixes

--- build/shaders/phong.glsl
+++ build/shaders/phong.glsl
@@ -4,14 +4,18 @@ in lowp vec3 v_normal;
 in lowp vec3 v_fragPos;
 in lowp vec3 v_cameraPos;
 out highp vec4 color;
+float rand(vec2 co) {
+	return fract(sin(dot(co.xy, vec2(12.9898, 78.233))) * 43758.5453);
+}
 void main () {
 	vec3 lightColor = vec3(1.0f, 1.0f, 1.0f);
 	vec3 objectColor = vec3(1.0f, 0.2f, 0.2f);
 	float ambientStrength = 0.1f;
 	float specularStrength = 1.0f;
 	float metallic = 128;
+	float randStrength = 0.5f;
 	
-	vec3 normal = normalize(v_normal);
+	vec3 normal = normalize(vec3(v_normal.x + rand(v_fragPos.xy) * randStrength, v_normal.y + rand(v_fragPos.yz) * randStrength, v_normal.z + rand(v_fragPos.xz) * randStrength));
 	vec3 lightDirection = normalize(vec3(1.0f, 1.0f, 1.0f) - v_fragPos);
 	
 	vec3 ambient = ambientStrength * lightColor;
@@ -22,7 +26,7 @@ void main () {
 	vec3 halfVector = normalize(lightDirection + viewDir);
 	vec3 specular = specularStrength * pow(max(dot(normal, halfVector), 0.0), metallic) * lightColor;
 	
-	vec3 light = (ambient + diffuse + specular) * objectColor;
+	vec3 light = (diffuse) * objectColor;
 	
 	color = vec4(light, 1.0f);
 }
