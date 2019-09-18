6c83122dc01243aaa146e586edfad12b77fc1633
MazeOfEncryption <inbox@encryptedmaze.net>
2019-09-18 12:57:29 -0400

    Minor changes

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -16,21 +16,21 @@ float DistributionGGX(vec3 N, vec3 H, float roughness) {
 	return num / denom;
 }
 float GeometrySchlickGGX(float NdotV, float roughness) {
-	float r = (roughness + 1.0);
-	float k = (r * r) / 8.0;
+	float r = (roughness + 1.0f);
+	float k = (r * r) / 8.0f;
 	float num = NdotV;
-	float denom = NdotV * (1.0 - k) + k;
+	float denom = NdotV * (1.0f - k) + k;
 	return num / denom;
 }
 float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness) {
 	float NdotV = max(dot(N, V), 0.0);
 	float NdotL = max(dot(N, L), 0.0);
-	float ggx2  = GeometrySchlickGGX(NdotV, roughness);
-	float ggx1  = GeometrySchlickGGX(NdotL, roughness);
+	float ggx2 = GeometrySchlickGGX(NdotV, roughness);
+	float ggx1 = GeometrySchlickGGX(NdotL, roughness);
 	return ggx1 * ggx2;
 }
 vec3 fresnelSchlick(float cosTheta, vec3 F0) {
-	return F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0);
+	return F0 + (1.0f - F0) * pow(1.0f - cosTheta, 5.0f);
 }
 void main () {
 	vec3 lightPositions[4] = vec3[4](
@@ -39,15 +39,15 @@ void main () {
 		vec3( 3.0f, 2.0f, -2.5f),
 		vec3(-3.0f, 2.0f, -5.0f)
 	);
-	vec3 lightColor = vec3(1.0f, 1.0f, 1.0f);
+	vec3 lightColor = vec3(10.0f);
 	
 	vec3 albedo = vec3(1.0f, 0.0f, 0.0f);
-	float metallic = 0.75f, roughness = 0.4f, ao = 1.0f;
+	float metallic = 0.5f, roughness = 0.625f, ao = 0.0f;
 
 	vec3 N = normalize(v_normal);
 	vec3 V = normalize(v_cameraPos - v_fragPos);
 
-	vec3 F0 = vec3(0.03f);
+	vec3 F0 = vec3(0.003f);
 	F0 = mix(F0, albedo, metallic);
 
 	vec3 Lo = vec3(0.0f);
@@ -60,18 +60,18 @@ void main () {
 		vec3 radiance = lightColor * attenuation;
 		float NDF = DistributionGGX(N, H, roughness);
 		float G = GeometrySmith(N, V, L, roughness);
-		vec3 F = fresnelSchlick(max(dot(H, V), 0.0), F0);
-		vec3 kS = F;
-		vec3 kD = vec3(1.0) - kS;
-		kD *= 1.0 - metallic;
+		vec3 F = fresnelSchlick(max(dot(H, V), 0.0f), F0);
 		vec3 numerator = NDF * G * F;
-		float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0);
-		vec3 specular = numerator / max(denominator, 0.001);
-		float NdotL = max(dot(N, L), 0.0);
+		float denominator = 4.0f * max(dot(N, V), 0.0f) * max(dot(N, L), 0.0f);
+		vec3 specular = numerator / max(denominator, 0.001f);
+		vec3 kS = F;
+		vec3 kD = vec3(1.0f) - kS;
+		kD *= 1.0f - metallic;
+		float NdotL = max(dot(N, L), 0.0f);
 		Lo += (kD * albedo / PI + specular) * radiance * NdotL;
 	}
 	// End of Per Light Calculations
-	vec3 ambient = vec3(0.03) * albedo * ao;
+	vec3 ambient = vec3(0.0f) * albedo * ao;
 	vec3 surface = ambient + Lo;
 	
 	surface = surface / (surface + vec3(1.0f));
