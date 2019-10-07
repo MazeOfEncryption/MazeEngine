b3f8f77d2a10624e41c5dc6b04c1afc1424b4635
MazeOfEncryption <inbox@encryptedmaze.net>
2019-10-07 11:09:32 -0400

    Initialized CMake

---
+++ build/CMakeFiles/MazeEngine.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MazeEngine.dir/main.cpp.obj"
+  "MazeEngine.pdb"
+  "MazeEngine.exe"
+  "MazeEngine.exe.manifest"
+  "libMazeEngine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MazeEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/MazeEngine.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MazeEngine.
+# This may be replaced when dependencies are built.
