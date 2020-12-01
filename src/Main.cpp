02d0c59d7cfcb4989a872e67371f2958d5fb3081
MazeOfEncryption <inbox@encryptedmaze.net>
2020-12-01 13:49:21 -0500

    fix: Update Main.cpp to have example usage of Render.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,14 +1,21 @@
-#define _USE_MATH_DEFINES
 #include "Scene.hpp"
 #include "Transform.hpp"
+#include "Mesh.hpp"
+#include "Shader.hpp"
+#include "Render.hpp"
 // #include "Object.hpp"
 // #include "Component.hpp"
 
 using namespace MazeEngine;
 int main () {
 	Scene scene;
-	Object *player = scene.createObject<Object>();
-	Component *transform = player->createComponent<Transform>();
+	Object    *box             = scene.createObject<Object>();
+	Transform *boxTransform    = box->createComponent<Transform>(0.0f, 0.5f, 5.0f);
+	Mesh      *boxMesh         = box->createComponent<Mesh>("object.ply");
+	Shader    *boxShader       = box->createComponent<Shader>("vert.glsl", "static.glsl");
+	Render    *boxRender       = box->createComponent<Render>(boxTransform, boxMesh, boxShader);
+	Object    *player          = scene.createObject<Object>();
+	Component *playerTransform = player->createComponent<Transform>();
 	scene.init();
 	return 0;
 }
