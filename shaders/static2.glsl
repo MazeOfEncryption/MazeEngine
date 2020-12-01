2e8dfa402f9134f3ae80c1b603f11df35a019597
MazeOfEncryption <inbox@encryptedmaze.net>
2020-12-01 17:42:14 -0500

    fix: Temporarily alter shaders to work without any uniform inputs

--- shaders/static2.glsl
+++ shaders/static2.glsl
@@ -1,6 +1,8 @@
 #version 330 core
 in highp vec2 v_UV;
+in highp vec3 v_normal;
 out highp vec4 color;
 void main () {
-	color = vec4(v_UV, 0.0f, 1.0f);
+	// color = vec4((v_normal + vec3(1.0f)) / 2.0f, 1.0f);
+	color = vec4(v_UV, 1.0f, 1.0f);
 }
--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -1,7 +1,7 @@
 #version 330 core
 in vec3 a_vertices;
 in vec3 a_normals;
-in vec3 a_UVs;
+in vec2 a_UVs;
 uniform mat4 u_worldMatrix;
 
 out vec3 v_normal;
@@ -11,7 +11,7 @@ out vec2 v_UV;
 void main () {
 	v_normal = a_normals;
 	v_fragPos = a_vertices;
-	v_UV = a_UVs.xy;
+	v_UV = a_UVs;
 	
-	gl_Position = u_worldMatrix * vec4(a_vertices, 1.0f);
+	gl_Position = vec4(a_vertices, 1.0f);
 }
