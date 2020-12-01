3a31fe1b4e29827779f03685c9d185d62e19013c
MazeOfEncryption <inbox@encryptedmaze.net>
2020-12-01 13:17:09 -0500

    feat: Add initializer list constructors to the Transform class

--- src/Transform.hpp
+++ src/Transform.hpp
@@ -5,11 +5,35 @@
 namespace MazeEngine {
 	class Transform : public Component {
 		public:
-			glm::vec3 position;
-			glm::vec3 rotation;
+			glm::vec3 pos;
+			glm::vec3 rot;
 			glm::vec3 scale;
-			// Transform() {};
-			// ~Transform() {};
+			Transform() {};
+			Transform(glm::vec3 pos)
+				: pos(pos)
+			{};
+			Transform(glm::vec3 pos, glm::vec3 rot)
+				: pos(pos)
+				, rot(rot)
+			{};
+			Transform(glm::vec3 pos, glm::vec3 rot, glm::vec3 scale)
+				: pos(pos)
+				, rot(rot)
+				, scale(scale)
+			{};
+			Transform(float px, float py, float pz)
+				: pos(glm::vec3(px, py, pz))
+			{};
+			Transform(float px, float py, float pz, float rx, float ry, float rz)
+				: pos(glm::vec3(px, py, pz))
+				, rot(glm::vec3(rx, ry, rz))
+			{};
+			Transform(float px, float py, float pz, float rx, float ry, float rz, float sx, float sy, float sz)
+				: pos(glm::vec3(px, py, pz))
+				, rot(glm::vec3(rx, ry, rz))
+				, scale(glm::vec3(sx, sy, sz))
+			{};
+			~Transform() {};
 			void init() {}
 			void update() {}
 		private:
