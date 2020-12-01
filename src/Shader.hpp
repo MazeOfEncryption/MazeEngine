d446eabfbe1beff621bca0c25c89ce4908040810
MazeOfEncryption <inbox@encryptedmaze.net>
2020-11-30 21:22:06 -0500

    refactor: Update Shader to use component architecture

--- src/Shader.hpp
+++ src/Shader.hpp
@@ -1,16 +1,19 @@
 #ifndef MAZEENGINE_SHADER
 #define MAZEENGINE_SHADER
 #include "Dep.hpp"
+#include "Component.hpp"
 #include "Read.hpp"
 #include <string>
 namespace MazeEngine {
-    class Shader {
+    class Shader : public Component {
         public:
-            Shader (std::string vert, std::string frag);
-            Shader ();
+            Shader(std::string vert, std::string frag);
+            Shader();
 			void setSource(std::string vert, std::string frag);
             void compile();
             unsigned getId();
+			void init();
+			void update() {}
         private:
             std::string vert, frag;
             unsigned shaderProgram;
