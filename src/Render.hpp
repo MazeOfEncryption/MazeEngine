2f4139548a3fc3c675d69d0aae7a14a35b4be15a
MazeOfEncryption <inbox@encryptedmaze.net>
2020-12-01 17:43:13 -0500

    feat: Introduce some basic functionality to the Render class

--- src/Render.hpp
+++ src/Render.hpp
@@ -1,31 +1,34 @@
 #ifndef MAZEENGINE_RENDER
 #define MAZEENGINE_RENDER
 #include "Component.hpp"
+#include "Shader.hpp"
+#include "Mesh.hpp"
 namespace MazeEngine {
 	class Transform;
-	class Mesh;
-	class Shader;
 	class Render : public Component {
 		public:
 			Render(Transform *transform)
 				: transform(transform)
-			{};
+			{}
 			Render(Transform *transform, Mesh *mesh)
 				: transform(transform)
 				, mesh(mesh)
-			{};
+			{}
 			Render(Transform *transform, Mesh *mesh, Shader *shader)
 				: transform(transform)
 				, mesh(mesh)
 				, shader(shader)
-			{};
-			~Render() {};
-			void init() {}
+			{}
+			Render() {}
+			~Render() {}
+			void init();
 			void update() {}
+			void draw();
 		private:
 			Transform *transform;
 			Mesh *mesh;
 			Shader *shader;
+			unsigned vao, vertexBuffer, normalBuffer, uvBuffer, a_vertices, a_normals, a_UVs;
 	};
 }
 #endif
